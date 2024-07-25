ea3a92bc30af8a07a9589b994beed6e2b7216e9c
Muhammad Shahzer <muhammadshahzer991@gmail.com>
2024-07-25 21:09:54 +0530

    changes to post ad

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import "./Stepper.css";
+import { ArrowBack } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import ripple from "../utils/ripple";
 
 const Stepper = ({ steps, current, onClick }) => {
+  const navigate = useNavigate();
   return (
     <div className="__stepp">
       <div className="stepper_container">
+        <button
+          className="back"
+          onClick={(e) => ripple(e, { dur: 1, cb: () => navigate("/") })}
+        >
+          <ArrowBack />
+        </button>
         {steps.map((step, index) => (
           <div
             onClick={() => onClick(index + 1)}
